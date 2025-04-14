bdd31ed75fc78d88aa15ca1fe1cde37c69a896fd
StylewithWicky <163636851+StylewithWicky@users.noreply.github.com>
2025-04-14 22:39:24 +0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './SearchBar.css'; // Create this file for styling
+import './SearchBar.css'; 
 
 function SearchBar({ searchTerm, setSearchTerm }) {
   return (
